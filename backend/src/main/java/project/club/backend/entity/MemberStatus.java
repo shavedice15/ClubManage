665b0381cea33a9643f00ebc23f6a2de6e6f4786
shavedice15 <s.i_fufusnow@hotmail.com>
2019-08-10 18:36:23 +0700

    close #6 - เพิ่มentityระบบสมัครเข้าชมรม

---
+++ backend/src/main/java/project/club/backend/entity/MemberStatus.java
@@ -0,0 +1,32 @@
+package project.club.backend.entity;
+import lombok.*;
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class MemberStatus {
+    @Id
+    @SequenceGenerator(name = "MemberStatus_seq", sequenceName = "MemberStatus_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MemberStatus_seq")
+    @Column(name="MemberStatus_ID")
+    private @NonNull long id;
+
+    private @NonNull String status;
+
+    public MemberStatus (String status){
+        this.status = status;
+    }
+    public long getId(){
+        return id;
+    }
+    public void setStatus(String status){
+        this.status = status;
+    }
+    public String getStatus(){
+        return status;
+    }
+}

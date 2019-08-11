ec89f4eacbd3ace7afc22695686f00dff9afdc80
shavedice15 <s.i_fufusnow@hotmail.com>
2019-08-11 16:55:44 +0700

    close #11 - update UI ระบบสมัครสมาชิกให้สมบูรณ์ขึ้น

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Apply from './components/ApplyToClub/Apply';
 import FindClub from './components/ApplyToClub/FindClub';
 import ClubInfo from './components/ApplyToClub/ClubInfo'
+import RegisClub from './components/ApplyToClub/RegisClub';
 class App extends Component {
   render() {
     return(
       <Router>
         <Switch>
           <Route path='/' exact={true} component={FindClub}/>
-          <Route path='/apply' exact={true} component={Apply}/>
           <Route path='/FindClub' exact={true} component={FindClub}/>
+          <Route path='/ClubInfo' exact={true} component={ClubInfo}/>
+          <Route path='/RegisClub' exact={true} component={RegisClub}/>
         </Switch>
       </Router>
     )
--- frontend/src/components/ApplyToClub/FindClub.js
+++ frontend/src/components/ApplyToClub/FindClub.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import '../../App.css';
 import AppNavbar from '../../AppNavbar';
 import { Link } from 'react-router-dom';
-import { Button, Container, Form, FormGroup, Input, Label } from 'reactstrap';
+import { Button, Container, Form, FormGroup, Input, Label, Table } from 'reactstrap';
 import OutlinedInput from '@material-ui/core/OutlinedInput';
 import FilledInput from '@material-ui/core/FilledInput';
 import InputLabel from '@material-ui/core/InputLabel';
@@ -10,6 +10,16 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+  }
+}));
+
 
 
 class FindClub extends Component {
@@ -30,31 +40,58 @@ class FindClub extends Component {
           <Form onSubmit={this.handleSubmit}>
             <div className="row">
                 <FormGroup className="col-md-6 mb-3">
-                <Label >ชื่อชมรม</Label>
-                <Input type="text" name="club" id="club" 
-                         autoComplete="club"/>
+                <TextField
+                    id="outlined-required"
+                    label="ชื่อชมรม"
+                    defaultValue=" "
+                    className={useStyles.textField}
+                    margin="normal"     
+                    variant="outlined"
+                  />
                 </FormGroup>
 
-                <FormGroup className="col-md-4 mb-3">
-                  <InputLabel htmlFor="tag-helper">Tag</InputLabel>
+                <FormGroup className="col-md-4 mb-3" >
+                <InputLabel htmlFor="tag-helper">Tag</InputLabel>
                   <Select 
                     value={this.state.tag}
                     onChange={this.handleChange}
+                    style={{ width: '45%',  textAlign: 'center'}}
+                    input={<OutlinedInput name="tag" id="outlined-tag-simple" />}
                   >
                     <MenuItem value="">
                     <em>None</em>
                     </MenuItem>
-                    <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem>
+                    <MenuItem value={10}>ดนตรี</MenuItem>
+                    <MenuItem value={20}>กีฬา</MenuItem>
+                    <MenuItem value={30}>ศาสนา</MenuItem>
                   </Select>
-                  <FormHelperText>Auto width</FormHelperText>
                 </FormGroup>
             </div>
             <FormGroup>
-                <Button color="primary" type="submit">ค้นหา</Button>{' '}
+                <Button style={{ background: '#000066' }} type="submit">ค้นหา</Button>
             </FormGroup>
-            </Form>
+          </Form>
+            <Table className="mt-4" >
+            <thead>
+            <tr align="center">
+              <th width="20%">ชื่อชมรม</th>
+              <th width="20%" >ประเภท</th>
+              <th width="10%">รายละเอียด</th>
+            </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>ดนตรีสากล</td>
+                <td align="center">ดนตรี</td>
+                <td align="center"><Button style={{ background: '#000066',width: '45%' }} tag={Link} to={"/ClubInfo"}>ดู</Button></td>
+              </tr>
+              <tr>
+                <td>สมาธิ</td>
+                <td align="center">ศาสนา</td>
+                <td align="center"><Button style={{ background: '#000066' ,width: '45%' }} tag={Link} to={"/ClubInfo"}>ดู</Button></td>
+              </tr>
+            </tbody>
+          </Table>
           </Container>
       </div>
     }

058ceb0037d55906a4f4867f090d4d9dadfc8d56
Thanchanit Tajeen <s.i_fufusnow@hotmail.com>
2019-09-23 00:58:15 +0700

    เพิ่ม firebase เก็บข้อมูลรูปภาพ
    เพิ่ม firebase เก็บข้อมูลรูปภาพ

---
+++ frontend/src/firebase/index.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/storage';
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB0iw2VunD_Nbg_mp2LaR91guLxF5L3KIQ",
+    authDomain: "sut523495-clubmanage.firebaseapp.com",
+    databaseURL: "https://sut523495-clubmanage.firebaseio.com",
+    projectId: "sut523495-clubmanage",
+    storageBucket: "sut523495-clubmanage.appspot.com",
+    messagingSenderId: "379608150322",
+    appId: "1:379608150322:web:a8844aeb63a848283da72b"
+};
+firebase.initializeApp(config);
+
+const storage = firebase.storage();
+
+export {
+    storage, firebase as default
+}
